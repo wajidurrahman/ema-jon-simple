4445341f2b2a5fae6b7e7499f5a372d25d50c66f
Wajidur Rahman <wrfuad0@gmail.com>
2021-04-18 12:17:19 +0600

    Review Component, Review Items, CSS in JavaScript style

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getDatabaseCart } from '../../utilities/databaseManager';
+import fakeData from '../../fakeData';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
+    const [cart, setCart] = useState([])
+    useEffect(()=>{
+        // cart
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
+
+        const cartProducts = productKeys.map(key => {
+           const product = fakeData.find(pd=> pd.key ===key);
+           product.quantity = savedCart[key];
+           return product;
+        });
+        setCart(cartProducts);
+    }, [])
     return (
         <div>
-            <h1>This is Review</h1>
+            <h1>Cart Items: {cart.length}</h1>
+            {
+                cart.map(pd => <ReviewItem 
+                    key={pd.key}
+                    product={pd}></ReviewItem>)
+            }
         </div>
     );
 };

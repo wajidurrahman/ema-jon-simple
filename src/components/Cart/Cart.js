08131a122e8d1703c69be09115a65a6f6e8811c8
Wajidur Rahman <wrfuad0@gmail.com>
2021-04-18 14:52:29 +0600

    React Developer Tool, Chrome extension, Refactor add to Cart.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,7 +8,7 @@ const Cart = (props) => {
     let total = 0;
     for(let i = 0; i < cart.length; i++){
         const product = cart[i];
-        total = total + product.price;
+        total = total + product.price * product.quantity;
     }
 
     let shipping = 0;
@@ -30,7 +30,7 @@ const Cart = (props) => {
         return Number(precision);
     }
     
-    return (
+    return ( 
         <div>
             <h4 className="bg-danger">Order Summary</h4>
             <p>Items Ordered: {cart.length}</p>
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { getDatabaseCart, removeFromDatabaseCart } from '../../utilities/databaseManager';
 import fakeData from '../../fakeData';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import Cart from '../Cart/Cart';
 
 const Review = () => {
     const [cart, setCart] = useState([]);
@@ -25,14 +26,18 @@ const Review = () => {
         setCart(cartProducts);
     }, [])
     return (
-        <div>
-            <h1>Cart Items: {cart.length}</h1>
+        <div className="twin-container">
+            <div className="product-container">
             {
                 cart.map(pd => <ReviewItem 
                     key={pd.key}
                     removeProduct = {removeProduct}
                     product={pd}></ReviewItem>)
             }
+            </div>
+            <div className="cart-container">
+              <Cart cart={cart}></Cart>
+            </div>
         </div>
     );
 };
